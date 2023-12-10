ed01fc6eb952789e088de6938931c07d9596e385
ImaLorikI <pasha.bandrivskiy@gmail.com>
2023-12-10 22:13:38 +0200

    task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,77 @@
-cons 
+const URL_BREED = 'https://api.thecatapi.com/v1/breeds';
+const URL_SEACRH_BREED ='https://api.thecatapi.com/v1/images/search?breed_ids=';
+const api_key = 'live_UYcNHMXMZUE8f3KQl2VWhf8LHKxL9n8ZxPDMX3MYZGFamrSUaM0QqEjYkFXdce9u';
+const URL_SEARCH = 'https://api.thecatapi.com/v1/images/search';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+function fetchBreeds() {
+  return fetch(URL_BREED, {
+    headers: {
+      'x-api-key': api_key,
+    },
+  }).then(response => {
+    if (!response.ok) { 
+      throw ('Mistake');
+    }
+    return response.json();
+  });
+}
+function fetchCatByBreed(breedId) { 
+  return fetch(URL_SEACRH_BREED+`${breedId}`, { 
+    headers: { 'x-api-key': api_key, }, 
+  }).then(response => { 
+    if (!response.ok) {
+      throw ('Mistake');
+    }
+    return response.json(); 
+  });
+}
+const BreedSelect = document.querySelector('.breed-select');
+BreedSelect.setAttribute('hidden', true);
+const CatInfo = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+const errorr = document.querySelector('.error');
+errorr.style.display = 'none';
+fetchBreeds() 
+  .then(data => {
+    const markUpSelect = data
+      .map(({ name, id }) => `<option value="${id}">${name}</option>`)
+      .join('');
+    BreedSelect.innerHTML = markUpSelect;
+    new SlimSelect({ 
+      select: '.breed-select', 
+    });
+    loader.style.display = 'none';
+    BreedSelect.removeAttribute('hidden');
+  })
+  .catch(error => {
+    loader.style.display = 'none';
+    errorr.style.display = 'block';
+    console.log(error);
+  });
+
+BreedSelect.addEventListener('change', evt => {
+  const IDBreed = evt.currentTarget.value;
+  CatInfo.innerHTML = '';
+  loader.style.display = 'block'; 
+  fetchCatByBreed(IDBreed)
+    .then(data => {
+      const markUpPhoto = data.map(({url,breeds: {0: { name, origin, description, temperament },},}) =>
+`<img src="${url}" alt="${name}" width="${500}" height="${250}">
+  <div class="js-some">
+  <h2 class="hmain-js" >${name}</h2>
+  <h3 class="hmain3-js">${origin}</h3>
+  <p class="text"> ${description}</p>
+  <p class= "text"> Temperament: ${temperament}</p>
+  </div>`
+        )
+        .join('');
+      loader.style.display = 'none';
+      CatInfo.innerHTML = markUpPhoto;
+    })
+    .catch(error => {
+      loader.style.display = 'none';
+      errorr.style.display = 'block';
+      console.log(error);
+    });
+});
